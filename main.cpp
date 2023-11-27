90e74768689146b7c62d81361cbe6f6adbfa257a
rapip <140580542+kupip@users.noreply.github.com>
2023-11-27 14:31:19 +0700

    Otak-atik
    Cobain printPapan nya tapa.

--- main.cpp
+++ main.cpp
@@ -13,24 +13,26 @@ int main(int argc, char** argv) {
 //	Player _tempPlayer1, _tempPlayer2;
 //	int jwb;
 
-	permainan temp_game;
-	int idx_halaman = 1;
-	tampilanAwal();
-	bool keluar = false;
-	do {
-		tampilHalaman(&idx_halaman, &temp_game, &keluar);
-	} while (keluar == false)
-//	jwb = getch();
-//	system("cls");
-//	
-//	inputNama(&_tempPlayer1);
-//	system("cls");
-	const char *dem = "";
-	tampilMenuAkhir(dem);
-	int jwb = getch();
-	while (jwb != 27) {
-//		dashboard(_tempPlayer1, _tempPlayer2);
-		jwb = getch();
-	}
+//	permainan temp_game;
+//	int idx_halaman = 1;
+//	tampilanAwal();
+//	bool keluar = false;
+//	do {
+//		tampilHalaman(&idx_halaman, &temp_game, &keluar);
+//	} while (keluar == false)
+////	jwb = getch();
+////	system("cls");
+////	
+////	inputNama(&_tempPlayer1);
+////	system("cls");
+//	const char *dem = "";
+//	tampilMenuAkhir(dem);
+//	int jwb = getch();
+//	while (jwb != 27) {
+////		dashboard(_tempPlayer1, _tempPlayer2);
+//		jwb = getch();
+//	}
+
+	printPapan(7);
 	return 0;
 }
