1649329c2f5b536f8184b71f646aa46681fe868a
AllThaf <thafafadillah@gmail.com>
2023-12-08 13:38:23 +0700

    Back
    Menambahkan fitur back dan memodif tampilan input nama

--- inputNama.cpp
+++ inputNama.cpp
@@ -9,20 +9,58 @@ Tgl			: 25/11/2023
 #include <windows.h>
 #include "header.h"
 
-void inputNama(permainan *game, int *index_halaman, int modeMain) {
+void inputNama(permainan *game, int *index_halaman, int modeMain)
+{
 	system("cls");
-	if (modeMain == 1) {
+	if (modeMain == 1)
+	{
+		banner();
 		kursorOut(45, 21);
 		printf("Masukkan nama anda: ");
 		scanf("%s", (*game).pemain1.nama);
 		strcpy((*game).pemain2.nama, "Computer");
 		*index_halaman = 7;
-	} else {
+	}
+	else
+	{
+		banner();
+		switch ((*game).jenis_papan)
+		{
+		case 3:
+			kursorOut(45, 20);
+			printf("Papan 3x3");
+			break;
+		case 5:
+			kursorOut(45, 20);
+			printf("Papan 5x5");
+			break;
+		case 7:
+			kursorOut(45, 20);
+			printf("Papan 7x7");
+			break;
+		}
 		kursorOut(45, 21);
 		printf("Masukkan nama Player 1: ");
-		scanf("%s", (*game).pemain1.nama);;
+		scanf("%s", (*game).pemain1.nama);
 		(*game).pemain1.skor = 0; // pemberian nilai awal buat skor biar ga kebawa skor yg lalu lalu
 		system("cls");
+
+		banner();
+		switch ((*game).jenis_papan)
+		{
+		case 3:
+			kursorOut(45, 20);
+			printf("Papan 3x3");
+			break;
+		case 5:
+			kursorOut(45, 20);
+			printf("Papan 5x5");
+			break;
+		case 7:
+			kursorOut(45, 20);
+			printf("Papan 7x7");
+			break;
+		}
 		kursorOut(45, 21);
 		printf("Masukkan nama Player 2: ");
 		scanf("%s", (*game).pemain2.nama);
--- pilihMode.cpp
+++ pilihMode.cpp
@@ -7,86 +7,105 @@ Tgl			: 30/11/2023
 #include <stdio.h>
 #include "header.h"
 #define W 87
-#define w 119 
+#define w 119
 #define S 83
 #define s 115
 #define atas 72
 #define bawah 80
 
-void pilihMode(int *index_halaman, int *modeMain) {
+void pilihMode(int *index_halaman, int *modeMain)
+{
 	system("cls");
 	int x, y, inp;
 	banner();
 	kursorOut(57, 13);
 	printf("Pilih Mode");
 	kursorOut(55, 15);
-  	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 201, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 187);
-  	kursorOut(55, 16);
-  	printf("%c vs Player    %c", 186, 186);
-  	kursorOut(55, 17);
-  	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 204, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 185);
-  	kursorOut(55, 18);
-  	printf("%c vs Computer  %c", 186, 186);
-  	kursorOut(55, 19);
-  	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 200, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 188);
-  	
-  	x = 53;
-  	y = 16;
-  	kursorOut(x,y);
-  	printf("%c", 175);
-  	do {
-  		inp = getch();
-		switch (inp) {
-			case atas:
-				printf("\b ");
-				if (y>16) {
-					y -= 2;
-				}
-				break;
-			case bawah:
-				printf("\b ");
-				if (y<18) {
-					y += 2;
-				}
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 201, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 187);
+	kursorOut(55, 16);
+	printf("%c vs Player    %c", 186, 186);
+	kursorOut(55, 17);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 204, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 185);
+	kursorOut(55, 18);
+	printf("%c vs Computer  %c", 186, 186);
+	kursorOut(55, 19);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 204, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 185);
+	kursorOut(55, 20);
+	printf("%c Back         %c", 186, 186);
+	kursorOut(55, 21);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 200, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 188);
+
+	x = 53;
+	y = 16;
+	kursorOut(x, y);
+	printf("%c", 175);
+	do
+	{
+		inp = getch();
+		switch (inp)
+		{
+		case atas:
+			printf("\b ");
+			if (y > 16)
+			{
+				y -= 2;
+			}
+			break;
+		case bawah:
+			printf("\b ");
+			if (y < 20)
+			{
+				y += 2;
+			}
 		}
-	
-		switch (inp) {
-			case W:
-				printf("\b ");
-				if (y>16) {
-					y -= 2;
-				}
-				break;
-			case S:
-				printf("\b ");
-				if (y<18) {
-					y += 2;
-				}
-			case w:
-				printf("\b ");
-				if (y>16) {
-					y -= 2;
-				}
-				break;
-			case s:
-				printf("\b ");
-				if (y<18) {
-					y += 2;
-				}
+
+		switch (inp)
+		{
+		case W:
+			printf("\b ");
+			if (y > 16)
+			{
+				y -= 2;
+			}
+			break;
+		case S:
+			printf("\b ");
+			if (y < 20)
+			{
+				y += 2;
+			}
+		case w:
+			printf("\b ");
+			if (y > 16)
+			{
+				y -= 2;
+			}
+			break;
+		case s:
+			printf("\b ");
+			if (y < 20)
+			{
+				y += 2;
+			}
 		}
-  	
+
 		kursorOut(x, y);
-		printf("%c", 175);  
-  } while (inp != 13);
-  
-  switch (y) {
-  	case 16:
-  		*index_halaman = 5;
-  		*modeMain = 2;
-  		break;
+		printf("%c", 175);
+	} while (inp != 13);
+
+	switch (y)
+	{
+	case 16:
+		*index_halaman = 5;
+		*modeMain = 2;
+		break;
 	case 18:
 		*index_halaman = 5;
 		*modeMain = 1;
 		break;
-  }
+	case 20:
+		system("cls");
+		*index_halaman = 1;
+		break;
+	}
 }
