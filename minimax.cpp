fb9f270f1a2662bce085bcbe927d8ace233c2499
rapip <140580542+kupip@users.noreply.github.com>
2023-12-09 10:50:42 +0700

    Update minimax.cpp

--- minimax.cpp
+++ minimax.cpp
@@ -189,3 +189,90 @@ posisi posisiTerbaik5(char papan[5][5]) {
 		}
 	return posTerbaik;
 }
+
+// Modul minimax untuk mengecek kemungkinan nilai dari sebuah gerakan
+int minimax7(char papan[7][7], int kedalaman, bool maximizing) {
+	int skor = cekPemenang7(papan);
+	int sementara;
+	if (skor == 1) {
+		return -1;
+	}
+	else if (skor == 2) {
+		return 1;
+	}
+	else if (skor == 3) {
+		return 0;
+	}
+	
+	if (maximizing)
+	{
+		int best = -10;
+		for (int i=0; i<7; i++)
+		{
+			for (int j=0; j<7; j++)
+			{
+				if (papan[i][j] == ' ')
+				{
+					papan[i][j] = 'O';
+					sementara = minimax7(papan, kedalaman+1, false);
+					
+					// meng-undo gerakan yang telah dibuat
+					papan[i][j] = ' ';
+					if (sementara > best)
+					{
+						best = sementara;
+					}
+				}
+			}
+		}
+		return best;
+	} else {
+		int best = 10;
+		for (int i=7; i<7; i++)
+		{
+			for (int j=7; j<7; j++)
+			{
+				if (papan[i][j] == ' ')
+				{
+					papan[i][j] = 'X';
+					sementara = minimax7(papan, kedalaman+1, true);
+					
+					// meng-undo gerakan yang telah dibuat
+					papan[i][j] = ' ';
+					if (sementara < best)
+					{
+						best = sementara;
+					}
+				}
+			}
+		}
+		return best;
+	}
+}
+
+// Modul untuk mencari posisi terbaik pada papan 7x7
+posisi posisiTerbaik7(char papan[7][7]) {
+	int best = -1000;
+	int sementara;
+	int depth = 0;
+	posisi posTerbaik;
+
+	for (int i=0; i<7; i++) {
+			for (int j=0; j<7; j++) {
+				if (papan[i][j] == ' ') {
+					papan[i][j] = 'O';
+					sementara = minimax7(papan, depth, false);
+					
+					// meng-undo gerakan yang telah dibuat
+					papan[i][j] = ' ';
+					if (sementara > best) {
+						best = sementara;
+						posTerbaik.i = i;
+						posTerbaik.j = j;
+					}
+					printf("%d ", sementara);
+				}
+			}
+		}
+	return posTerbaik;
+}
