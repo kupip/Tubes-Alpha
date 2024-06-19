41cbc6e9bbfe7742c0ee3fefede3cc17df9bf916
Rafif Genadratama <muhammad.rafif.tif423@polban.ac.id>
2024-06-19 10:06:46 +0700

    satuin menuAwal dan menuAkhir

---
+++ compile.bat
@@ -0,0 +1,23 @@
+ banner.cpp
+-a---l        30/05/2024     23.19           5233 computer.cpp
+-a---l        30/05/2024     22.56            550 dashboard.cpp
+-a----        19/06/2024     09.52            280 data.bin
+-a---l        19/06/2024     09.52          12797 header.h
+-a---l        30/05/2024     22.56           1385 howToPlay.cpp
+-a---l        30/05/2024     22.56           2217 inputNama.cpp
+-a---l        30/05/2024     23.14            410 kursorOut.cpp
+-a---l        30/05/2024     23.16            384 main.cpp
+-a---l        30/05/2024     22.56           5752 main3x3.cpp
+-a---l        30/05/2024     22.56           7547 main5x5.cpp
+-a---l        30/05/2024     22.56          11986 main7x7.cpp
+-a---l        30/05/2024     22.56            600 mainGame.cpp
+-a---l        30/05/2024     22.56           2538 menuAkhir.cpp
+menuAwal.cpp
+pilihMode.cpp
+pilihPapan.cpp
+printPapan.cpp
+saveLoad.cpp
+tampilanAwal.cpp
+tampilHalaman.cpp
+tampilHighScore.cpp
+timer.cpp
--- src/menu.cpp
+++ src/menu.cpp
@@ -125,3 +125,120 @@ void menuAkhir(permainan game, char pemenang[20], int *index_halaman, bool *kelu
 		break;
 	}
 }
+
+
+#define W 87
+#define w 119
+#define S 83
+#define s 115
+#define atas 72
+#define bawah 80
+
+void menuAwal(int *index_halaman, bool *keluar)
+{
+	int x, y, inp;
+	banner();
+	kursorOut(58, 13);
+	printf("Main Menu");
+	kursorOut(55, 15);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 201, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 187);
+	kursorOut(55, 16);
+	printf("%c    START    %c", 186, 186);
+	kursorOut(55, 17);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 204, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 185);
+	kursorOut(55, 18);
+	printf("%c  LOAD GAME  %c", 186, 186);
+	kursorOut(55, 19);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 204, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 185);
+	kursorOut(55, 20);
+	printf("%c  HIGHSCORE  %c", 186, 186);
+	kursorOut(55, 21);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 204, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 185);
+	kursorOut(55, 22);
+	printf("%c HOW TO PLAY %c", 186, 186);
+	kursorOut(55, 23);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 204, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 185);
+	kursorOut(55, 24);
+	printf("%c    EXIT     %c", 186, 186);
+	kursorOut(55, 25);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 200, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 188);
+
+	x = 53;
+	y = 16;
+	kursorOut(x, y);
+	printf("%c", 175);
+	do
+	{
+		inp = getch();
+		switch (inp)
+		{
+		case atas:
+			printf("\b ");
+			if (y > 16)
+			{
+				y -= 2;
+			}
+			break;
+		case bawah:
+			printf("\b ");
+			if (y < 24)
+			{
+				y += 2;
+			}
+		}
+
+		switch (inp)
+		{
+		case W:
+			printf("\b ");
+			if (y > 16)
+			{
+				y -= 2;
+			}
+			break;
+		case S:
+			printf("\b ");
+			if (y < 24)
+			{
+				y += 2;
+			}
+		case w:
+			printf("\b ");
+			if (y > 16)
+			{
+				y -= 2;
+			}
+			break;
+		case s:
+			printf("\b ");
+			if (y < 24)
+			{
+				y += 2;
+			}
+		}
+
+		kursorOut(x, y);
+		printf("%c", 175);
+	} while (inp != 13);
+
+	switch (y)
+	{
+	case 16:
+		*index_halaman = 4;
+		break;
+	case 18:
+		*index_halaman = 2;
+		break;
+	case 20:
+		*index_halaman = 3;
+		break;
+	case 22:
+		*index_halaman = 9;
+		break;
+	case 24:
+		*keluar = true;
+		break;
+	}
+	fflush(stdin);
+	system("cls");
+}
