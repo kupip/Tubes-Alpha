91b9d6ff0c4d34cdb1724416d83081139462b1b4
AllThaf <thafafadillah@gmail.com>
2024-06-20 08:16:38 +0700

    fix: inisialisasi

--- src/tampilHalaman.cpp
+++ src/tampilHalaman.cpp
@@ -7,44 +7,39 @@ Tgl			: 26/11/2023
 #include <stdio.h>
 #include "header.h"
 
-void tampilHalaman(int *index_halaman, bool *keluar) {
+void tampilHalaman(int *index_halaman, bool *keluar)
+{
 	permainan temp_game;
 	char pemenang[20];
-	
-	// Inisialisasi
-	temp_game.jenis_papan=0;
-	temp_game.modeMain=0;
-	strcpy(temp_game.pemain1.nama, " ");
-	temp_game.pemain1.skor=0;
-	strcpy(temp_game.pemain2.nama, " ");
-	temp_game.pemain2.skor=0;
-	switch(*index_halaman) {
-		case 1: // menu awal
-			menuAwal(&(*index_halaman), &(*keluar));
-			break;
-		case 2: // pilih opsi load game
-			loadGame(&(*index_halaman), &temp_game);
-			break;
-		case 3: // pilih opsi high score
-			tampilHighScore(&(*index_halaman));
-			break;
-		case 4: // input Nama player kalo pilih mode vs komputer
-			pilihMode(&(*index_halaman), &temp_game.modeMain);
-			break;
-		case 5: // buat milih jenis papan
-			pilihPapan(&(*index_halaman), &temp_game.jenis_papan);
-			break;
-		case 6:
-			inputNama(&temp_game, &(*index_halaman), temp_game.modeMain); 
-			break;
-		case 7:
-			mainGame(&temp_game, &pemenang, &(*index_halaman));
-			break;
-		case 8:
-			menuAkhir(temp_game, pemenang, &(*index_halaman), &(*keluar));
-			break;
-		case 9:
-			howToPlay(&(*index_halaman));
-			break;
+	Player high_score[20];
+	switch (*index_halaman)
+	{
+	case 1: // menu awal
+		menuAwal(&(*index_halaman), &(*keluar));
+		break;
+	case 2: // pilih opsi load game
+		loadGame(&(*index_halaman), &temp_game);
+		break;
+	case 3: // pilih opsi high score
+		tampilHighScore(&(*index_halaman));
+		break;
+	case 4: // input Nama player kalo pilih mode vs komputer
+		pilihMode(&(*index_halaman), &temp_game.modeMain);
+		break;
+	case 5: // buat milih jenis papan
+		pilihPapan(&(*index_halaman), &temp_game.jenis_papan);
+		break;
+	case 6:
+		inputNama(&temp_game, &(*index_halaman), temp_game.modeMain);
+		break;
+	case 7:
+		mainGame(&temp_game, &pemenang, &(*index_halaman));
+		break;
+	case 8:
+		menuAkhir(temp_game, pemenang, &(*index_halaman), &(*keluar));
+		break;
+	case 9:
+		howToPlay(&(*index_halaman));
+		break;
 	};
 }
